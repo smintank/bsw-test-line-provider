cb2f5000cfabbaae8c10d1a7260ac823160b6bd6
smintank <assbestmail@gmail.com>
2025-02-12 21:35:48 +0400

    Add lifespan with db init

--- bet-maker/main.py
+++ bet-maker/main.py
@@ -1,15 +1,25 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 import logging
 from fastapi import FastAPI
 
 from config import settings
-from api import router as bet_router
+from database import init_db
+from routes.bets import router as bet_router
+from routes.events import router as event_router
 
 logger = logging.getLogger(__name__)
 
 
-app = FastAPI(debug=settings.debug)
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan, debug=settings.debug)
 app.include_router(bet_router)
+app.include_router(event_router)
 
 
 if __name__ == "__main__":
