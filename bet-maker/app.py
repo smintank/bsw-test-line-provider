36bcc53b8621c3e02708d70aa980c3d6256f594f
smintank <assbestmail@gmail.com>
2025-02-12 13:39:59 +0400

    Add get events endpoint

--- bet-maker/app.py
+++ bet-maker/app.py
@@ -1,12 +1,16 @@
+import httpx
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-def get_bet():
-    pass
+@app.get("/events/")
+async def get_events():
+    async with httpx.AsyncClient() as client:
+        response = await client.get("http://fastapi-app:8080/events")
+    return response
+
 
 
 if __name__ == "__main__":
