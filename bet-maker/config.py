0da643014c7ffdbb70511aeec95293c0219c7903
smintank <assbestmail@gmail.com>
2025-02-12 23:04:57 +0400

    Refactor config.py add some variables from settings

--- bet-maker/config.py
+++ bet-maker/config.py
@@ -1,15 +1,19 @@
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-
 class Settings(BaseSettings):
-    postgres_url: str
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
     debug: bool
+    bet_maker_host: str
+    bet_maker_port: int
     events_url: str | None = None
     is_single: bool = True
 
-    model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=False, extra="allow")
 
 
 settings = Settings()
--- bet-maker/main.py
+++ bet-maker/main.py
@@ -1,21 +1,14 @@
-from contextlib import asynccontextmanager
+import logging
 
 import uvicorn
-import logging
 from fastapi import FastAPI
 
 from config import settings
-from database import init_db
+from database import lifespan
 from routes.bets import router as bet_router
 from routes.events import router as event_router
 
 logger = logging.getLogger(__name__)
-
-
-@asynccontextmanager
-async def lifespan(application: FastAPI):
-    await init_db()
-    yield
 
 app = FastAPI(lifespan=lifespan, debug=settings.debug)
 app.include_router(bet_router)
@@ -23,6 +16,4 @@ app.include_router(event_router)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    logger.info("Starting bet maker")
-    uvicorn.run(app, host="0.0.0.0", port=8888)
+    uvicorn.run(app, host=settings.bet_maker_host, port=settings.bet_maker_port)
