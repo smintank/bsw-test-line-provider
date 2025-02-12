7dc53ec97699b863c2d36de4d77f121dcbfc2054
smintank <assbestmail@gmail.com>
2025-02-12 21:33:03 +0400

    Add config flags

--- bet-maker/config.py
+++ bet-maker/config.py
@@ -2,9 +2,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+
 class Settings(BaseSettings):
     postgres_url: str
     debug: bool
+    events_url: str | None = None
+    is_single: bool = True
 
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
 
