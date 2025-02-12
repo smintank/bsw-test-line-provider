02e5300318b419503178ddbc1361d9d5099c312b
smintank <assbestmail@gmail.com>
2025-02-12 21:38:59 +0400

    Implement cruds

--- bet-maker/crud.py
+++ bet-maker/crud.py
@@ -1,14 +1,31 @@
-from schemas import SCreateBet, SBet, SEvent, EventState
+from httpx import AsyncClient
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config import settings
+from models import Bet
+from schemas import SCreateBet, SEvent, EventState
 
 
-async def create_bet(bet: SCreateBet) -> int:
-    return bet.event_id
+async def create_bet(db: AsyncSession, bet: SCreateBet) -> int:
+    new_bet = Bet(amount=bet.amount, event_id=bet.event_id)
+    db.add(new_bet)
+    await db.commit()
+    await db.refresh(new_bet)
+    return new_bet.id
 
 
-async def get_all_bets() -> list[SBet] | None:
-    return [SBet(bet_id=1, amount=2.3, status=2), SBet(bet_id=2, amount=3.33, status=1)]
+async def get_all_bets(db: AsyncSession) -> list[Bet]:
+    result = await db.execute(select(Bet))
+    return list(result.scalars().all())
 
 
-async def fetch_all_events() -> list[SEvent] | None:
-    return [SEvent(event_id=1, status=EventState.FINISHED_WIN, coefficient=1.43, deadline=234234234234),
-            SEvent(event_id=1, status=EventState.FINISHED_LOSE, coefficient=1.43, deadline=234234234234)]
+async def fetch_all_events() -> list[SEvent]:
+    if settings.is_single:
+        return [SEvent(event_id=1, status=EventState.FINISHED_WIN, coefficient=1.43, deadline=234234234234),
+                SEvent(event_id=1, status=EventState.FINISHED_LOSE, coefficient=1.43, deadline=234234234234)]
+    url = settings.events_url
+    async with AsyncClient() as client:
+        response = await client.get(url)
+        events = await response.json()
+        return events
