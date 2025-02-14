58dd9ed1eca8a9f5626fe35d6dfe6e9cbf02d5a7
smintank <assbestmail@gmail.com>
2025-02-14 13:10:12 +0400

    Improve event api request logic

--- bet-maker/services/event_services.py
+++ bet-maker/services/event_services.py
@@ -10,24 +10,31 @@ logger = logging.getLogger(__name__)
 
 
 class LineProviderService:
-
     BASE_URL = settings.event_app_url
 
-    @staticmethod
-    async def get_url_data(url: str) -> dict:
-        async with AsyncClient() as client:
-            try:
-                response = await client.get(url)
-                response.raise_for_status()
-                return response.json()
-            except DecodingError as e:
-                logger.error("[%s] Ошибка декодирования JSON: %s", url, e)
-            except HTTPStatusError as e:
-                logger.error("[%s] Ошибка HTTP: %s", url, e)
-            except RequestError as e:
-                logger.error("[%s] Ошибка запроса: %s", url, e)
-            except Exception as e:
-                logger.error("[%s] Непредвиденная ошибка: %s", url, e)
+    _client: Optional[AsyncClient] = None
+
+    @classmethod
+    def get_client(cls) -> AsyncClient:
+        if cls._client is None:
+            cls._client = AsyncClient()
+        return cls._client
+
+    @classmethod
+    async def get_url_data(cls, url: str) -> dict:
+        client = cls.get_client()
+        try:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.json()
+        except DecodingError as e:
+            logger.error("[%s] Ошибка декодирования JSON: %s", url, e)
+        except HTTPStatusError as e:
+            logger.error("[%s] Ошибка HTTP: %s", url, e)
+        except RequestError as e:
+            logger.error("[%s] Ошибка запроса: %s", url, e)
+        except Exception as e:
+            logger.error("[%s] Непредвиденная ошибка: %s", url, e)
         return {}
 
     @classmethod
@@ -39,3 +46,9 @@ class LineProviderService:
     async def fetch_available_events(cls) -> List[EventSchema]:
         data = await cls.get_url_data(cls.BASE_URL)
         return [EventSchema(**event) for event in data] if data else []
+
+    @classmethod
+    async def close_client(cls):
+        if cls._client:
+            await cls._client.aclose()
+            cls._client = None
